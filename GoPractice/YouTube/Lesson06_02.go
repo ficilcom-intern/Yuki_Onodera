95b301ea6917913c1b5bc7ab447dc2a73fd21801
Yuki Onodera <(maroonv4869@icloud.com)>
2023-04-07 15:13:44 +0900

    add: Lesson06(変数)

---
+++ GoPractice/YouTube/Lesson06_02.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num = 2
+	fmt.Println(num)
+}
---
+++ GoPractice/YouTube/Lesson06_03.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := 3
+	fmt.Println(num)
+}
