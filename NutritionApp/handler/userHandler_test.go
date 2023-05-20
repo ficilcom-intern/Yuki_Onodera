30588b204d65cf9b00f4a067bb56a8a02e346010
Yuki Onodera <(maroonv4869@icloud.com)>
2023-05-20 12:14:34 +0900

    implement userHandler_test

--- NutritionApp/handler/userHandler_test.go
+++ NutritionApp/handler/userHandler_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/stretchr/testify/mock"
 
 	"kunikida123456/NutritionApp/domain/model"
-	"kunikida123456/NutritionApp/myerror"
 )
 
 // モックのUserUsecase
@@ -22,9 +21,6 @@ type mockUserUsecase struct {
 
 // Signupメソッドのモック
 func (m *mockUserUsecase) Signup(name, email, password string) (*model.User, error) {
-	if email == "" || password == "" {
-		return nil,  &myerror.BadRequestError{Msg: "empty password"}
-	}
 	args := m.Called(name, email, password)
 	return args.Get(0).(*model.User), args.Error(1)
 }
@@ -40,9 +36,9 @@ func TestSignup(t *testing.T) {
 
 	// モックの戻り値を設定（成功の場合）
 	mockUsecase.On("Signup", "John Doe", "john@example.com", "password123").Return(&model.User{
-		ID:    1,
-		Name:  "John Doe",
-		Email: "john@example.com",
+		ID:       1,
+		Name:     "John Doe",
+		Email:    "john@example.com",
 		Password: "password123",
 	}, nil)
 
@@ -67,26 +63,36 @@ func TestSignup(t *testing.T) {
 	expectedResponse := `{"id":1,"name":"John Doe","email":"john@example.com"}`
 	assert.JSONEq(t, expectedResponse, rec.Body.String())
 }
-func TestSignup_EmptyPassword(t *testing.T) {
+
+func TestLogin(t *testing.T) {
 	// モックのUserUsecaseを作成
 	mockUsecase := &mockUserUsecase{}
 
-	mockUsecase.On("Signup", "John Doe", "john@example.com").Return(nil, &myerror.BadRequestError{Msg: "empty password"})
+	// モックの戻り値を設定（成功の場合）
+	mockUsecase.On("Login", "john@example.com", "password123").Return("dummy-signed-string", &model.User{
+		ID:    1,
+		Name:  "John Doe",
+		Email: "john@example.com",
+	}, nil)
 
 	// テストケース実行
 	reqBody := `{
-		"name": "John Doe",
 		"email": "john@example.com",
-		"password": ""
+		"password": "password123"
 	}`
-	req := httptest.NewRequest("POST", "/users/signup", strings.NewReader(reqBody))
+	req := httptest.NewRequest("POST", "/users/login", strings.NewReader(reqBody))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := echo.New().NewContext(req, rec)
 	h := NewUserHandler(mockUsecase)
-	err := h.Signup(c)
+	err := h.Login(c)
 
 	// 結果の検証
-	assert.EqualError(t, err, "email and password are required")
-	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
+
+	expectedResponse := `{"id":1,"name":"John Doe","Token":"dummy-signed-string"}`
+
+
+	assert.JSONEq(t, expectedResponse, rec.Body.String())
 }
