18b8f1f32a545219a6e212a19357f897aadb5146
Yuki Onodera <(maroonv4869@icloud.com)>
2023-05-24 18:40:29 +0900

    implement infra_test

--- NutritionApp/domain/model/meal.go
+++ NutritionApp/domain/model/meal.go
@@ -6,16 +6,15 @@ import (
 )
 
 type Meal struct {
-	ID        int        `json:"id" gorm:"primaryKey"`
-	UserID    int        `json:"user_id" gorm:"references:UserID"`
-	Memo      string     `json:"memo" gorm:"default:null"`
-	MealType  string     `json:"type" gorm:"unique;not null"`
-	Carbs     float64    `json:"carbs" gorm:"default:null"`
-	Fat       float64    `json:"fat" gorm:"default:null"`
-	Protein   float64    `json:"protein" gorm:"default:null"`
-	Calories  float64    `json:"calories" gorm:"default:null"`
-	CreatedAt time.Time  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	ID        int       `json:"id" gorm:"primaryKey"`
+	UserID    int       `json:"user_id" gorm:"references:UserID"`
+	Memo      string    `json:"memo" gorm:"default: null"`
+	MealType  string    `json:"type" gorm:"default: null"`
+	Carbs     float64   `json:"carbs" gorm:"default: null"`
+	Fat       float64   `json:"fat" gorm:"default:null"`
+	Protein   float64   `json:"protein" gorm:"default:null"`
+	Calories  float64   `json:"calories" gorm:"default:null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
 
 func NewMeal(userID int, memo string, mealType string, carbs float64, fat float64, protein float64, calories float64) (*Meal, error) {
